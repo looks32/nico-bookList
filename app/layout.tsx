0a2b418e194c7a82d4f04d0beede1ed494b953a4
Dang <56210432+looks32@users.noreply.github.com>
2024-03-29 00:19:08 +0900

    배포 테스트
    배포 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "../css/app.css";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Library",
+    template: "%s | 하세기!",
     default: "Library",
   },
   description: "The best movies on the best framework",
